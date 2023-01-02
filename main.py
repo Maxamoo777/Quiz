5dc8c1d917b7e3d6444537a286ce932bba843f96
Max Mason <max.mason796@gmail.com>
2023-01-02 08:21:34 -0800

    new queston

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# This is the welcome message.
 print("Welcome...TO THE IMPOSSIBLE QUIZ!")
 score = 0
 
@@ -8,5 +9,9 @@ if answer == "4":
 
 answer = input("7x7=? ")
 if answer == "49":
+    score = score + 1
+    print("Correct! Your score is now " + str(score) + "!")
+answer = input("3/6=? ")
+if answer == "0.5":
     score = score + 1
     print("Correct! Your score is now " + str(score) + "!")
