5ce7c299a94ff31c7fa6230fd5716dc0f490ff94
Max Mason <max.mason796@gmail.com>
2023-01-02 08:57:08 -0800

    Loops questons

--- main.py
+++ main.py
@@ -2,17 +2,37 @@
 print("Welcome...TO THE IMPOSSIBLE QUIZ!")
 score = 0
 
+answering = True
 answer = input("2+2=?")
-if answer == "4":
-    score = score + 1
-    print("Correct! Your score is now " + str(score) + "!")
 
-answer = input("7x7=? ")
-if answer == "49":
-    score = score + 1
-    print("Correct! Your score is now " + str(score) + "!")
-    
+while answering == True:
+    if answer == "4":
+        score = score + 1
+        print("Correct! Your score is now " + str(score) + "!")
+    else:
+        print("Incorrect.")
+        answer = input("2+2=?")
+
+
+answering = True
+answer = input("7x7=?" )
+
+while answering == True:
+    if answer == "49":
+        score = score + 1
+        print("Correct! Your score is now " + str(score) + "!")
+    else:
+        print("Incorrect.")
+        answer = input("7x7=? ")
+
+
+answering = True
 answer = input("3/6=? ")
-if answer == "0.5":
-    score = score + 1
-    print("Correct! Your score is now " + str(score) + "!")
+
+while answering == True:
+    if answer == "0.5":
+        score = score + 1
+        print("Correct! Your score is now " + str(score) + "!")
+    else:
+        print("Incorrect.")
+        answer = input("3/6=? ")  
