9bbb8266f4d4a2dae55631b79911a550afb441a8
Max Mason <max.mason796@gmail.com>
2023-01-02 08:16:42 -0800

    changed spaceing

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 print("Welcome...TO THE IMPOSSIBLE QUIZ!")
 score = 0
 
-answer = input("2 + 2 = ? ")
+answer = input("2+2=?")
 if answer == "4":
     score = score + 1
     print("Correct! Your score is now " + str(score) + "!")
 
-answer = input("7 x 7 = ? ")
+answer = input("7x7=? ")
 if answer == "49":
     score = score + 1
     print("Correct! Your score is now " + str(score) + "!")
