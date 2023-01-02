a0e19621fd55ebadc637d4a8abfb9caaca3dd1ce
Max Mason <max.mason796@gmail.com>
2023-01-02 08:35:58 -0800

    fix

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ answer = input("7x7=? ")
 if answer == "49":
     score = score + 1
     print("Correct! Your score is now " + str(score) + "!")
+    
 answer = input("3/6=? ")
 if answer == "0.5":
     score = score + 1
